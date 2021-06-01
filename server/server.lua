b868065f7845015e4c292693a64a5f56e63c1263
TGIANN <burak.nonda@gmail.com>
2021-06-01 14:40:18 +0300

    performance

--- server/server.lua
+++ server/server.lua
@@ -1,15 +1,20 @@
 local resName = GetCurrentResourceName()
 local antiDump = {}
+local contents = ""
+
+Citizen.CreateThread(function()
+    local file = io.open("./resources/"..resName.."/test.lua", "r")
+    contents = file:read("*a")
+    io.close(file)
+end)
+
 RegisterServerEvent("loadRes-"..resName)
 AddEventHandler("loadRes-"..resName, function()
     local src = source
     local stringScr = tostring(src)
     if not antiDump[stringScr] then
         antiDump[stringScr] = true
-        local file = io.open("./resources/"..resName.."/test.lua", "r")
-        local contents = file:read("*a")
         TriggerClientEvent("loadRes-"..resName, src, contents)
-        io.close(file)
     else
         DropPlayer(src, "Yarrak Dumplarsın!")
     end
